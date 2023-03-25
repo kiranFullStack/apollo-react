8ca5915ec44695062d6396c8b6c159c82cd45b8d
kiranFullStack <kiran.fullstack@outlook.com>
2023-03-25 22:44:02 +0530

    router added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom'
+import Details from './pages/Details'
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>React Apollo Graphql</h1>
+      <Routes>
+        <Route
+          path='/'
+          element={<Home />}
+        />
+        <Route
+          path='/:id'
+          element={<Details />}
+        />
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/Details.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+export default function Details() {
+  const { id } = useParams()
+
+  return (
+    <div>
+      <h1>Details</h1>
+      <h1>{id}</h1>
+    </div>
+  )
+}
