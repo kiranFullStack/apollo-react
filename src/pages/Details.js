3be4cb19607f518a25fcaf749895dc986792520f
kiranFullStack <kiran.fullstack@outlook.com>
2023-03-25 23:30:05 +0530

    final code

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,13 +1,26 @@
+import { gql, useQuery } from '@apollo/client'
 import React from 'react'
 import { useParams } from 'react-router-dom'
+
+const COUNTRY = gql`
+  query Country($code: ID!) {
+    country(code: $code) {
+      name
+    }
+  }
+`
 
 export default function Details() {
   const { id } = useParams()
 
+  const { data, error, loading } = useQuery(COUNTRY, {
+    variables: { code: id },
+  })
+
   return (
     <div>
       <h1>Details</h1>
-      <h1>{id}</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import { useQuery, gql } from '@apollo/client'
+import { RingLoader } from 'react-spinners'
+import { Link } from 'react-router-dom'
+
+const GET_COUNTRIES = gql`
+  {
+    countries {
+      name
+      code
+      emoji
+      currency
+      continent {
+        name
+        code
+      }
+    }
+  }
+`
 
 export default function Home() {
+  const { data, error, loading } = useQuery(GET_COUNTRIES)
   return (
     <div>
-      <h1>Home</h1>
+      {loading && (
+        <RingLoader
+          color='rgba(54, 60, 214, 0.67)'
+          size={200}
+          speedMultiplier={2}
+        />
+      )}
+      {error && <h1>{<pre>{JSON.stringify(error, null, 2)}</pre>}</h1>}
+      {data && (
+        <>
+          {data.countries.map((item) => (
+            <div key={item.code}>
+              <h1>{item.name}</h1>
+              <h1>{item.emoji}</h1>
+              <Link to={`/${item.code}`}>Details</Link>
+            </div>
+          ))}
+        </>
+      )}
     </div>
   )
 }
